be916aec53fe36ca69deaf7746b13560f177ec52
Yoan Tournade <y@yoantournade.com>
2025-04-11 19:58:40 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md") as f:
 
 setup(
     name="tqwgp-parser",
-    version="0.6.7",
+    version="0.7.0",
     url="https://github.com/YtoTech/talk-quote-work-getpaid-parser",
     license="AGPL-3.0",
     author="Yoan Tournade",
